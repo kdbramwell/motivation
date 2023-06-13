a36670da4f72d54bd0effe3c7d787fcfad5c7de4
Kamal Bramwell <kdbramwell@gmail.com>
2023-06-13 12:03:09 -0400

    Updated app name

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -63,7 +63,7 @@ kotlin {
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication.common"
+    namespace = "com.kamalbramwell.motivation"
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
