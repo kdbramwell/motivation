5c01b35436f8c834e0a6d30b68ce0103449eda61
Kamal Bramwell <kdbramwell@gmail.com>
2023-06-12 20:44:56 -0400

    Updated project files from compose-multiplatform-template

--- androidApp/src/androidMain/kotlin/com/kamalbramwell/motivation/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/kamalbramwell/motivation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myapplication
+package com.kamalbramwell.motivation
 
 import MainView
 import android.os.Bundle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "MyApplication"
+rootProject.name = "ResoluteMan"
 
 include(":androidApp")
 include(":shared")
